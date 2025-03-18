539813ea709635179357d24dd76d72e789e3eb4a
Evgenij Novikov <novikov.ev84@yandex.ru>
2025-03-18 16:10:14 +0300

    Удалена лишняя зависимость.

--- src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
@@ -1,11 +1,11 @@
 package ru.yandex.practicum.filmorate;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import ru.yandex.practicum.filmorate.controller.FilmController;
 import ru.yandex.practicum.filmorate.controller.UserController;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
